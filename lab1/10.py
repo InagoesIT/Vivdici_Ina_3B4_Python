7be47d81eaa47c792d7ca036ae80e2f8588b9622
ina <ina@debian-BULLSEYE-live-builder-AMD64>
2022-10-10 13:51:15 +0300

    ex10 done

--- lab1/10.py
+++ lab1/10.py
@@ -2,11 +2,15 @@ def get_nr_of_words(characters):
     nr_of_words = 1
     index = characters.find(' ')
 
-    while index != -1 and index < len(characters) - 1:
-        if characters[index + 1] != ' ':
+    while index != -1:
+        if index < len(characters) - 1 and characters[index + 1] != ' ':
             nr_of_words += 1
-        characters = characters[index + 1:]
+            characters = characters[index + 1:]
+        else:
+            while characters.find(' ') != -1:
+                characters = characters[1:]
+        index = characters.find(' ')
 
     return nr_of_words
 
-print(get_nr_of_words("heaaaa a d d"))
+print(get_nr_of_words("heaaaa a d d         g      aa"))
